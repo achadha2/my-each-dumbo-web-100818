3e08ea440a13bec5e67abe341dd8f9cd14a700f5
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-09-12 04:03:43 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+while i < array.length do
+   yield(array[i])
+   i += 1
+end
+return array
+end
+
+
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+    puts array[i]
+  end
 end
