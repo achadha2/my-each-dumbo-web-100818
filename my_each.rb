a69ea7a65bbc4fb7a61428741f0a8cc106a0f5fb
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-09-12 04:03:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,17 +1,14 @@
 def my_each(array)
   i = 0
 while i < array.length do
-   yield[array]
+   yield(array[i])
    i += 1
 end
 return array
 end
 
-my_array = ["happy", "sad", "silly"]
-my_each(my_array)
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+    puts array[i]
   end
 end
